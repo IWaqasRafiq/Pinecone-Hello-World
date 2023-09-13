b47675945e661e4595a28aefae733c311a08d097
waqas rafiq <wm66179@gmail.com>
2023-09-14 01:46:13 +0500

    some work

--- routes/post.mjs
+++ routes/post.mjs
@@ -1,22 +1,18 @@
 
 import express from 'express';
-import { nanoid } from 'nanoid'
-import { client } from './../mongodb.mjs'
-import { ObjectId } from 'mongodb'
+import { customAlphabet } from 'nanoid'
+import { ObjectId } from 'mongodb';
+import pineconeClient, { openai as openaiClient }
+    from './../pinecone.mjs';
 
-const db = client.db("cruddb");
-const col = db.collection("posts");
+const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10)
+
+const pcIndex = pineconeClient.Index(process.env.PINECONE_INDEX_NAME);
+console.log("process.env.PINECONE_INDEX_NAME: ", process.env.PINECONE_INDEX_NAME);
 
 let router = express.Router()
 
-// not recommended at all - server should be stateless
-let posts = [
-    {
-        id: nanoid(),
-        title: "abc post title",
-        text: "some post text"
-    }
-]
+
 
 // POST    /api/v1/post
 router.post('/post', async (req, res, next) => {
@@ -37,81 +33,114 @@ router.post('/post', async (req, res, next) => {
     }
 
     try {
-        const insertResponse = await col.insertOne({
-            // _id: "7864972364724b4h2b4jhgh42",
-            title: req.body.title,
-            text: req.body.text,
-            createdOn: new Date()
-        });
-        console.log("insertResponse: ", insertResponse);
 
-        res.send('post created');
+        const response = await openaiClient.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: `${req.body.title} ${req.body.text}`,
+        });
+        const vector = response?.data[0]?.embedding
+        console.log("vector: ", vector);
+
+        const upsertResponse = await pcIndex.upsert([{
+            id: nanoid(), 
+            values: vector,
+            metadata: {
+                title: req.body.title,
+                text: req.body.text,
+                createdOn: new Date().getTime()
+            },
+        }]);
+        console.log("upsertResponse: ", upsertResponse);
+
+        res.send({ message: 'post created' });
     } catch (e) {
         console.log("error inserting mongodb: ", e);
-        res.status(500).send('server error, please try later');
+        res.status(500).send({ message: 'server error, please try later' });
     }
 })
 
 
 router.get('/posts', async (req, res, next) => {
 
-    const cursor = col.find({})
-        .sort({ _id: -1 })
-        .limit(100);
-
     try {
-        let results = await cursor.toArray()
-        console.log("results: ", results);
-        res.send(results);
+        const response = await openaiClient.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: "",
+        });
+        const vector = response?.data[0]?.embedding
+        console.log("vector: ", vector);
+
+        const queryResponse = await pcIndex.query({
+            vector: vector,
+            // id: "vec1",
+            topK: 10000,
+            includeValues: true,
+            includeMetadata: true
+        });
+
+        queryResponse.matches.map(eachMatch => {
+            console.log(`score ${eachMatch.score.toFixed(1)} => ${JSON.stringify(eachMatch.metadata)}\n\n`);
+        })
+        console.log(`${queryResponse.matches.length} records found `);
+
+        const formattedOutput = queryResponse.matches.map(eachMatch => ({
+            text: eachMatch?.metadata?.text,
+            title: eachMatch?.metadata?.title,
+            _id: eachMatch?.id,
+        }))
+
+        res.send(formattedOutput);
+
     } catch (e) {
-        console.log("error getting data mongodb: ", e);
+        console.log("error getting data pinecone: ", e);
         res.status(500).send('server error, please try later');
     }
+
 })
 
 
-// [92133,92254, 92255 ]
-
-router.get('/post/:postId', async (req, res, next) => {
-    console.log('this is signup!', new Date());
-
-    if (!ObjectId.isValid(req.params.postId)) {
-        res.status(403).send(`Invalid post id`);
-        return;
-    }
-
-
-    // const cursor = col.find({ price: { $lte: 77 } });
-    // const cursor = col.find({
-    //     $or: [
-    //         { _id: req.params.postId },
-    //         { title: "dfsdf sdfsdf" }
-    //     ]
-    // })
-
+router.get('/search', async (req, res, next) => {
 
     try {
-        let result = await col.findOne({ _id: new ObjectId(req.params.postId) });
-        console.log("result: ", result); // [{...}] []
-        res.send(result);
+        const response = await openaiClient.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: req.query.q,
+        });
+        const vector = response?.data[0]?.embedding
+        console.log("vector: ", vector);
+
+        const queryResponse = await pcIndex.query({
+            vector: vector,
+            // id: "vec1",
+            topK: 20,
+            includeValues: false,
+            includeMetadata: true
+        });
+
+        queryResponse.matches.map(eachMatch => {
+            console.log(`score ${eachMatch.score.toFixed(3)} => ${JSON.stringify(eachMatch.metadata)}\n\n`);
+        })
+        console.log(`${queryResponse.matches.length} records found `);
+
+        const formattedOutput = queryResponse.matches.map(eachMatch => ({
+            text: eachMatch?.metadata?.text,
+            title: eachMatch?.metadata?.title,
+            _id: eachMatch?.id,
+        }))
+
+        res.send(formattedOutput);
+
     } catch (e) {
-        console.log("error getting data mongodb: ", e);
+        console.log("error getting data pinecone: ", e);
         res.status(500).send('server error, please try later');
     }
+
 })
 
-// PUT     /api/v1/post/:postId
-// {
-//     title: "updated title",
-//     text: "updated text"
-// }
+
 
 router.put('/post/:postId', async (req, res, next) => {
 
-    if (!ObjectId.isValid(req.params.postId)) {
-        res.status(403).send(`Invalid post id`);
-        return;
-    }
 
     if (!req.body.text
         && !req.body.title) {
@@ -125,45 +154,44 @@ router.put('/post/:postId', async (req, res, next) => {
         `)
     }
 
-    let dataToBeUpdated = {};
-
-    if (req.body.title) { dataToBeUpdated.title = req.body.title }
-    if (req.body.text) { dataToBeUpdated.text = req.body.text }
-
-
     try {
-        const updateResponse = await col.updateOne(
-            {
-                _id: new ObjectId(req.params.postId)
-            },
-            {
-                $set: dataToBeUpdated
-            });
-        console.log("updateResponse: ", updateResponse);
+        const response = await openaiClient.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: `${req.body.title} ${req.body.text}`,
+        });
+        const vector = response?.data[0]?.embedding
+        console.log("vector: ", vector);
 
-        res.send('post updated');
+        const upsertResponse = await pcIndex.upsert([{
+            id: req.params.postId,
+            values: vector,
+            metadata: {
+                title: req.body.title,
+                text: req.body.text,
+            },
+        }]);
+        console.log("upsertResponse: ", upsertResponse);
+
+
+        res.send({ message: 'post created' });
     } catch (e) {
         console.log("error inserting mongodb: ", e);
-        res.status(500).send('server error, please try later');
+        res.status(500).send({ message: 'server error, please try later' });
     }
+
+
+
+
 })
 
-// DELETE  /api/v1/post/:userId/:postId
+// DELETE  /api/v1/post/:postId
 router.delete('/post/:postId', async (req, res, next) => {
 
-    if (!ObjectId.isValid(req.params.postId)) {
-        res.status(403).send(`Invalid post id`);
-        return;
-    }
 
-    try {
-        const deleteResponse = await col.deleteOne({ _id: new ObjectId(req.params.postId) });
-        console.log("deleteResponse: ", deleteResponse);
-        res.send('post deleted');
-    } catch (e) {
-        console.log("error deleting mongodb: ", e);
-        res.status(500).send('server error, please try later');
-    }
+    const deleteResponse = await pcIndex.deleteOne(req.params.postId)
+    console.log("deleteResponse: ", deleteResponse);
+
+    res.send('post deleted');
 })
 
 export default router
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider,ColorModeScript,theme } from "@chakra-ui/react";
+import App from "./App";
+import ColorModeSwitcher from './ColorModeSwitcher.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
+    <ColorModeScript/>
+    <ChakraProvider theme={theme}>
+    <ColorModeSwitcher />
+    </ChakraProvider>
   </React.StrictMode>
 );
-
